ef90ba4d36557934d456b4b857de453618475728
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-02-04 21:25:02 +0000

    update to dark-mode for dropdown menu and info section

--- src/Components/Information/Info.jsx
+++ src/Components/Information/Info.jsx
@@ -12,17 +12,15 @@ function mode() {
 
   // Theme context
   const { isDarkMode, setIsDarkMode} = useContext(ThemeContext);
+  
 
 
   return (
     <>
       <div className="InfoArea">
-      <h2>Description Sections</h2>
-      <div className="g">
-    <div>bla</div>
-</div>
-{/* Add content for the description section here */}
-<div><span>text</span><span /></div>
+              <h2 className="Title">Analyse</h2>
+              <h2 className="Title">Detect</h2>
+              <h2 className="Title">Improve</h2>
       </div>
     </>
   )
