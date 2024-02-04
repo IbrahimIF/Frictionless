2a57bd345ba0c2b224ca3d596cbd212b271f04ea
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-02-04 20:33:02 +0000

    dropdown menu has been made and applied

--- src/Components/Main-area/Buttons/Extra-button/extra.jsx
+++ src/Components/Main-area/Buttons/Extra-button/extra.jsx
@@ -1,24 +1,53 @@
 
 import './extra.css'
-import '../../../../Darkmode.css';
-import { useContext, useEffect } from "react";
-import ThemeContext from "../../../Contexts/ThemeContext";
+import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark,faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 function extra() {
 // Theme context
-const { isDarkMode, setIsDarkMode} = useContext(ThemeContext);
+const [selectedLanguage, setSelectedLanguage] = useState('Choose Language');
+const [selectedTheme, setSelectedTheme] = useState('Choose Theme');
 
+const [languageDropdownOpen, setLanguageDropdownOpen] = useState(false);
+const [themeDropdownOpen, setThemeDropdownOpen] = useState(false);
+
+const toggleLanguageDropdown = () => setLanguageDropdownOpen(!languageDropdownOpen);
+const toggleThemeDropdown = () => setThemeDropdownOpen(!themeDropdownOpen);
 
 
   return (
     <>
 <div className="buttonBottomArea">
-<button className="button">Clear All <FontAwesomeIcon icon= {faXmark} /></button>
-<button className="button">Choose Language</button>
-<button className="button">Choose Theme</button>
-<button className="check-button">Check</button>
+  <button className="button">Clear All <FontAwesomeIcon icon= {faXmark} /></button>
+  
+  <button className="button" onClick={toggleLanguageDropdown}>{selectedLanguage} <FontAwesomeIcon icon= {faCaretDown} />
+  {languageDropdownOpen && (
+    <div className="dropdown-content" style={{ display: languageDropdownOpen ? 'flex' : 'none' }}>
+      <button className="dropdown" onClick={() => setSelectedLanguage('Javascript')}>Javascript</button>
+      <button className="dropdown" onClick={() => setSelectedLanguage('Java')}>Java</button>
+      <button className="dropdown" onClick={() => setSelectedLanguage('Typescript')}>Typescript</button>
+      <button className="dropdown" onClick={() => setSelectedLanguage('Python')}>Python</button>
+      <button className="dropdown" onClick={() => setSelectedLanguage('C#')}>C#</button>
+      <button className="dropdown" onClick={() => setSelectedLanguage('C++')}>C++</button>
+    </div>
+    )}
+  </button>
+
+
+  <button className="button" onClick={toggleThemeDropdown}>{selectedTheme} <FontAwesomeIcon icon= {faCaretDown} />
+  {themeDropdownOpen && (
+    <div className="dropdown-content" style={{ display: themeDropdownOpen ? 'flex' : 'none' }}>
+      <button className="dropdown" onClick={() => setSelectedTheme('Dracular Official')}>Draculat Official</button>
+      <button className="dropdown" onClick={() => setSelectedTheme('One Dark Pro')}>One Dark Pro</button>
+      <button className="dropdown" onClick={() => setSelectedTheme('Cobalt2')}>Cobalt2</button>
+      <button className="dropdown" onClick={() => setSelectedTheme('Night Owl')}>Night Owl</button>
+      <button className="dropdown" onClick={() => setSelectedTheme('Atom One Dark')}>Atom One Dark</button>
+      <button className="dropdown" onClick={() => setSelectedTheme('GithubTheme')}>GithubTheme</button>
+    </div>
+    )}
+  </button>
+  <button className="check-button"><span>check</span><span>done</span></button>
 </div>
     </>
   )
