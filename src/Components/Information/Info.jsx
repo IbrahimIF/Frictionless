527b261966d66c4bc7b8de6a8bafe1673bda07ed
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-02-19 21:52:51 +0000

    50% of the infoSection has been completed

--- src/Components/Information/Info.jsx
+++ src/Components/Information/Info.jsx
@@ -3,6 +3,12 @@
 
 import './Info.css';
 import '../../Darkmode.css';
+
+import LightLogo from '../../assets/Images/Frictionless Logo light.png';
+import DarkLogo from '../../assets/Images/Frictionless Logo dark.png';
+
+import Cube from '../../assets/Images/3D Cube.png';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import ThemeContext from "../Contexts/ThemeContext";
@@ -17,22 +23,27 @@ function mode() {
   return (
     <>
       <div className="infoArea">
-        <div className="titleArea">
-          <h2 className="Title">Analyse</h2>
-          <h2 className="Title">Detect</h2>
-          <h2 className="Title">Improve</h2>
+        <div className="title"><img src={isDarkMode ? DarkLogo : LightLogo} className="titleSize" alt="Logo" /></div>
+        <div className="textArea">
+          <p className="textInfo">A text with a text inside a text, more simplified words,<br />and more text</p>
         </div>
-        <div className="titleArea">
-          <p className="texts">A text with a text inside a text, more simplified words,<br />and more text</p>
-        </div>
-        <div className="cardArea">
-          <div className="card">
-            <div className="main-content">
-              <p className="text">tedtddnwjnd</p>
+        <div className="belowArea">
+          <div className="leftArea">
+            <div className="leftCube"><img src={Cube} className="cubeSize" alt="3Dcube" /></div>
+            <div className="analyseArea">
+              <h1 className="textInfo">Analyse</h1>
             </div>
-            <div className="second-content">
-              <div className="test" style={{backgroundPosition: "center 40%", borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
-              </div>
+          </div>
+          <div className="centralArea">
+            <div className="centralCube"><img src={Cube} className="cubeSize" alt="3Dcube" /></div>
+            <div className="detectArea">
+              <h1 className="textInfo">Detect</h1>
+            </div>
+          </div>
+          <div className="rightArea">
+            <div className="rightCube"><img src={Cube} className="cubeSize" alt="3Dcube" /></div>
+            <div className="improveArea">
+            <h1 className="textInfo">Improve</h1>
             </div>
           </div>
         </div>
